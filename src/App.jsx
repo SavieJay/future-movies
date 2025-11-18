ca362a8627e815545f2db507964bff95b6967d49
SAVIE <savyj03@gmail.com>
2025-11-18 17:49:15 +0100

    almost ready for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Search from "./components/search";
 import Spinner from "./components/spinner";
 import MovieCard from "./components/movieCard";
 import { useDebounce } from "react-use";
+import { updateSearchCount, getTrendingMovies } from "./appwrite";
 
 const API_BASE_URL = 'https://api.themoviedb.org/3/';
 
@@ -23,10 +24,11 @@ const App = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
     const [movieList, setMovieList] = useState([]);
+    const [trendingMovies, setTrendingMovies] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
 
-    useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
+    useDebounce(() => setDebouncedSearchTerm(searchTerm), 800, [searchTerm]);
 
     // console.log(movieList);
     
@@ -58,6 +60,10 @@ const App = () => {
             }
             setMovieList(data.results || []);
 
+            if (query && data.results.length > 0) {
+                await updateSearchCount(query, data.results[0]);
+            }
+
         } catch (error) {
             console.log(`Error fetching movies: ${error}`);
             setErrorMessage('Error fetching movies, please try again later')
@@ -66,9 +72,23 @@ const App = () => {
         }
     }
 
+    const loadTrendingMovies = async () => {
+        try {
+            const movies = await getTrendingMovies();
+
+            setTrendingMovies(movies || []);
+        } catch (error) {
+            console.log(`Error loading trending movies: ${error}`);
+        }
+    }
+
     useEffect(() => {
         fetchMovies(debouncedSearchTerm)
-    }, [debouncedSearchTerm])
+    }, [debouncedSearchTerm]);
+
+    useEffect(() => {
+        loadTrendingMovies();
+    }, []);
 
     return ( 
         <main>
@@ -81,8 +101,23 @@ const App = () => {
 
                     <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
                 </header>
+
+                {trendingMovies.length > 0 && (
+                    <section className="trending">
+                        <h2>Trending Movies</h2>
+                        <ul>
+                            {trendingMovies.map((movie, index) => (
+                                <li key={movie.$id}>
+                                    <p>{index + 1}</p>
+                                    <img src={movie.poster_url} alt={movie.title} />
+                                </li>
+                            ))}
+                        </ul>
+                    </section>
+                )}
+
                 <section className="all-movies">
-                    <h2 className="mt-10">All movies</h2>
+                    <h2>All movies</h2>
                     
 
                     {isLoading ? (
