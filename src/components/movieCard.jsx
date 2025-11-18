2e1bec50b1fc145650596877b62b90cbff04d085
SAVIE <savyj03@gmail.com>
2025-11-18 14:47:32 +0100

    first commit

--- src/components/movieCard.jsx
+++ src/components/movieCard.jsx
@@ -2,6 +2,8 @@ const MovieCard = ({ movie:
     { title, vote_average, poster_path, release_date, original_language } 
 }) => {
     return ( 
+
+        // Movie Card Component
         <div className="movie-card">
             <img 
                 src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : 'No-Poster.png'} 
